48673f656aaecf83a3f59c71a96f249ea9a5d1ef
Fadeltnt <101524603+Fadeltnt@users.noreply.github.com>
2024-09-13 22:32:23 -0400

    deploy config

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={clientId}>
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <GlobalStateProvider>
             <DevSupport ComponentPreviews={ComponentPreviews}
                         useInitialHook={useInitial}
